9a68523f0006f9d10df239d341d2082bcd641369
s-rishu <rishu.singh1203@gmail.com>
2024-04-20 20:34:54 -0400

    added cse driver function

--- optimizations/subexp_elim.ml
+++ optimizations/subexp_elim.ml
@@ -1,3 +1,17 @@
 open Available
+open Cfg_ast
 
 exception Implement_Me
+(* function to perform CSE for a given block *)
+let subexp_elim_block (b: block) (f_acc: func) (avail_map: avail_in_out): func = raise Implement_Me
+
+
+(* driver to perform common subexpression elimination *)
+let subexp_elim (f : func) : func = 
+  let avail_map = Available.available_expression f in
+  let rec subexp_elim_helper (f_acc : func) (f_tail : func) : func = (
+    match f_tail with
+      | [] -> f_acc
+      | hd :: tl -> subexp_elim_helper (subexp_elim_block hd f_acc avail_map) tl  
+  ) in
+  (subexp_elim_helper f f)
